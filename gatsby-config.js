c6fe846afcedcaa115c84aa007e13e87e9282dea
yamam00s <s.yamamo0327@gmail.com>
2019-12-29 02:39:38 +0900

    ts導入

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,13 @@
 
 module.exports = {
   /* Your site config here */
+  plugins: [
+    `gatsby-plugin-typescript`,
+    {
+      resolve: 'gatsby-plugin-graphql-codegen',
+      options: {
+        fileName: `types/graphql-types.d.ts`
+      }
+    }
+  ]
 }
