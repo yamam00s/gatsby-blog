b54cc8751dc3b32c22e9f837626ddd09f087dd74
yamam00s <s.yamamo0327@gmail.com>
2019-12-30 13:57:42 +0900

    add layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 const Component: React.FC = () => (
-  <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+  <Layout>
     <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
     <p>
       What do I like to do? Lots of course but definitely enjoy building
       websites.
     </p>
-  </div>
+  </Layout>
 )
 
 export default Component
