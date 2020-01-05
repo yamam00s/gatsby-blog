dc14a8bb196456aaccef40329c30295f6f9dd001
yamam00s <s.yamamo0327@gmail.com>
2020-01-05 23:32:16 +0900

    create 動的ページ用テンプレート

---
+++ src/templates/blog-post.tsx
@@ -0,0 +1,13 @@
+
+import React from "react"
+import Layout from "../components/layout"
+
+const Component: React.FC = () => {
+  return (
+    <Layout>
+      <div>Hello blog post</div>
+    </Layout>
+  )
+}
+
+export default Component
