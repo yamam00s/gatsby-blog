e51439d45019ff8c4456f96fd8d53762b5376456
yamam00s <s.yamamo0327@gmail.com>
2020-01-19 22:10:48 +0900

    tagのリンク設置

--- src/templates/tag-index.tsx
+++ src/templates/tag-index.tsx
@@ -1,11 +1,17 @@
 import React from "react"
 import Layout from "../components/Layout/layout"
+import { TagIndexPageContext } from "../../gatsby-node/createPages"
 
-const Component: React.FC = () => {
+type TagIndexData = {
+  pageContext: TagIndexPageContext
+}
+
+const Component: React.FC<TagIndexData> = ({ pageContext }) => {
+  const { tag } = pageContext
   return (
     <Layout>
       <div>
-        tagページです
+        {tag}の記事一覧
       </div>
     </Layout>
   )
